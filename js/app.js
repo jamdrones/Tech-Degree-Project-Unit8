aabb2d62fa9a980ad9fe8dfbbf836c5ad5b503c1
jamdrones <76794064+jamdrones@users.noreply.github.com>
2021-04-12 09:55:37 -0700

    Update to CSS

--- js/app.js
+++ js/app.js
@@ -69,10 +69,8 @@ fetch(urlAPI)
 
 gridContainer.addEventListener('click', e => {
     
-    //make sure the click is not on the gridContainer itself
+    
     if(e.target !== gridContainer) {
-
-        // select the card element based on its proximity to actual element clicked
 
         const card = e.target.closest('.card');
         const index = card.getAttribute('data-index');
